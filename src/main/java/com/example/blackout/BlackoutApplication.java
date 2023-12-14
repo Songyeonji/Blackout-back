ce01640696db515ccd22d2f397fc7bb7f64cd86b
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-12-14 15:51:18 +0900

    스프링 백 서버

---
+++ src/main/java/com/example/blackout/BlackoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blackout/BlackoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
